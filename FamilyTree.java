7d9ef3f55379d7cff570130fc8048af21dc27c9f
gatlinglolx <shahov199@gmail.com>
2023-12-11 17:25:29 +0300

    FamilyTree_1

---
+++ FamilyTree.java
@@ -0,0 +1,11 @@
+public class FamilyTree {
+    private Person root;
+
+    public FamilyTree(Person root) {
+        this.root = root;
+    }
+
+    public Person getRoot() {
+        return root;
+    }
+}
